1f86d7fc2299e0578df4b08ae5d1677cd6a75444
Mouadh Mkadmi <36245246+wvagen@users.noreply.github.com>
2022-01-29 15:50:35 +0100

    Version 7.4

--- Assets/MyAssets/MyScripts/MyAlertCanvas.cs
+++ Assets/MyAssets/MyScripts/MyAlertCanvas.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class MyAlertCanvas : MonoBehaviour
+public class MyAlertCanvas : MonoBehaviour 
 {
+    public MyManager myMan;
+
     public Animator myAnim;
     public Text alertTitleTxt, alertMsgTxt;
     public AudioSource myAudioSource;
+    public AudioSource musicPlayer;
 
-    public AudioClip infoPanelAudioClip,hoverBtnSFX;
+    public AudioClip infoPanelAudioClip,hoverBtnSFX, musicSfxSwitch;
 
-    public static bool isSfxOn = true;
-    public static bool isMusicOn = true;
+    public Image musicImg, sfxImg;
+
+    public Sprite musicBtnOnSprite,musicBtnOffSprite;
+
+    public static bool musicOn = true;
+    public static bool sfxOn = true;
 
     GameObject hoveredGO;
+
+    private void Start()
+    {
+        SoundsStats();
+    }
 
     public void Display_Alert(string title,string msg)
     {
@@ -26,22 +38,104 @@ public class MyAlertCanvas : MonoBehaviour
     public void Display_Alert(string msg)
     {
         alertMsgTxt.text = msg;
-        if (isSfxOn)
+        if (sfxOn)
         {
             myAudioSource.PlayOneShot(infoPanelAudioClip);
         }
         myAnim.Play("Display_Alert_Anim");
     }
 
-    public void Hover_Btn()
+    public void Hover_Btn(GameObject eventData)
     {
         myAudioSource.PlayOneShot(hoverBtnSFX);
-        hoveredGO = EventSystem.current.currentSelectedGameObject;
+        hoveredGO = eventData.gameObject;
         hoveredGO.transform.localScale = Vector3.one * 1.2f;
     }
 
     public void UnHover_Btn()
     {
-        hoveredGO.transform.localScale = Vector2.one ;
+        if (hoveredGO != null)
+        {
+            hoveredGO.transform.localScale = Vector2.one;
+        }
+    }
+
+    void SoundsStats()
+    {
+        musicOn = (PlayerPrefs.GetInt("Sound_Enabled", 1) == 1);
+
+        musicPlayer.mute = !musicOn;
+        myAudioSource.mute = !sfxOn;
+
+        if (musicOn)
+        {
+            musicImg.sprite = musicBtnOnSprite;
+        }
+        else
+        {
+            musicImg.sprite = musicBtnOffSprite;
+        }
+
+        if (sfxOn)
+        {
+            sfxImg.sprite = musicBtnOnSprite;
+        }
+        else
+        {
+            sfxImg.sprite = musicBtnOffSprite;
+        }
+    }
+
+    public void MusicBtn()
+    {
+        myAudioSource.PlayOneShot(musicSfxSwitch);
+        if (musicOn)
+        {
+            musicOn = false;
+            musicImg.sprite = musicBtnOffSprite;
+            PlayerPrefs.SetInt("Sound_Enabled", 0);
+        }
+        else
+        {
+            musicOn = true;
+            musicImg.sprite = musicBtnOnSprite;
+            PlayerPrefs.SetInt("Sound_Enabled", 1);
+        }
+        musicPlayer.mute = !musicOn;
+    }
+
+    public void Pause_Btn()
+    {
+        Time.timeScale = 0;
+        myAnim.Play("PausePanel");
+    }
+
+    public void Resume()
+    {
+        myAnim.Play("Resume_Anim");
+        Time.timeScale = 1;
+    }
+
+    public void Settings_Btn()
+    {
+        myAnim.Play("Display_Settings");
+    }
+
+    public void SfxBtn()
+    {
+        myAudioSource.PlayOneShot(musicSfxSwitch);
+
+        if (sfxOn)
+        {
+            sfxOn = false;
+            sfxImg.sprite = musicBtnOffSprite;
+            myAudioSource.mute = true;
+        }
+        else
+        {
+            sfxOn = true;
+            sfxImg.sprite = musicBtnOnSprite;
+            myAudioSource.mute = false;
+        }
     }
 }
